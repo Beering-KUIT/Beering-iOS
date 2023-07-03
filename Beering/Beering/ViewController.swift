7d8a9ec62eb56cfbce35f5938d2afab82c207ad9
YoonSub Lim <yslim37@naver.com>
2023-07-03 12:01:21 +0900

    Add: Initialize Project

---
+++ Beering/Beering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beering
+//
+//  Created by YoonSub Lim on 2023/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
