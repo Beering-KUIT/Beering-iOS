3c740b049ed93e993954dfc538f6b716d8332056
YoonSub Lim <yslim37@naver.com>
2023-07-05 11:32:00 +0900

    Feat: Tab Bar Controller 구현

--- Beering/Beering/ViewController.swift
+++ Beering/Beering/ViewController.swift
@@ -18,6 +18,11 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
 
+
+    }
+    
+    @IBAction func loginRequestBtnTap(_ sender: Any) {
+        
         let LoginStoryboard = UIStoryboard(name: "Login", bundle: nil)
         let LoginVC = LoginStoryboard.instantiateInitialViewController()
         // 메인 화면을 풀 스크린으로 표시
@@ -25,11 +30,26 @@ class ViewController: UIViewController {
         self.present(LoginVC!, animated: true)
         
         // 윈도우의 rootViewController로 설정하여 메인 화면 표시
-        UIApplication.shared.windows.first?.rootViewController = LoginVC
-        UIApplication.shared.windows.first?.makeKeyAndVisible()
+//        UIApplication.shared.windows.first?.rootViewController = LoginVC
+//        UIApplication.shared.windows.first?.makeKeyAndVisible()
 
     }
+    
+    
+    @IBAction func loginDoneBtnTap(_ sender: Any) {
+        
+        let TabBarStoryboard = UIStoryboard(name: "TabBar", bundle: nil)
+        let TabBarVC = TabBarStoryboard.instantiateInitialViewController()
+        // 메인 화면을 풀 스크린으로 표시
+        TabBarVC?.modalPresentationStyle = .fullScreen
+        self.present(TabBarVC!, animated: true)
+        
+        // 윈도우의 rootViewController로 설정하여 메인 화면 표시
+//        UIApplication.shared.windows.first?.rootViewController = HomeVC
+//        UIApplication.shared.windows.first?.makeKeyAndVisible()
 
+    }
+    
 }
 
 struct VCPreView:PreviewProvider {
