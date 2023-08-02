6527e0c0bf0786d30ac0617261fa0e7e8ea6d5c6
YoonSub Lim <yslim37@naver.com>
2023-08-02 15:20:41 +0900

    Feat: 리뷰상세 리뷰이미지 컬렉션 뷰 구성

--- Beering/Beering/Search/Detail/ReviewDetail/ReviewDetailVC.swift
+++ Beering/Beering/Search/Detail/ReviewDetail/ReviewDetailVC.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ReviewDetailVC: UIViewController {
+    
+    var tempImageData:[String] = ["https://picsum.photos/347",
+                                  "https://picsum.photos/347",
+                                  "https://picsum.photos/347",
+                                  "https://picsum.photos/347",
+                                  "https://picsum.photos/347"]
+    
 
+    @IBOutlet weak var reviewImageCollectionView: UICollectionView!
+    
     @IBOutlet weak var reviewDetailText: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        reviewImageCollectionView.dataSource = self
+        reviewImageCollectionView.delegate = self
         
         reviewDetailText.textViewInit()
         
@@ -20,6 +32,31 @@ class ReviewDetailVC: UIViewController {
 
     }
     
+}
 
+extension ReviewDetailVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tempImageData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reviewImageCell", for: indexPath) as! ReviewImageCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reviewDetailImageCell", for: indexPath) as! ReviewDetailImageCell
+        
+        let imageUrl = tempImageData[indexPath.row]
+        cell.reviewDetailImageView.loadImage(from: imageUrl)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
+//        return CGSize(width: (self.reviewImageCollectionView?.frame.width)!, height: (self.reviewImageCollectionView?.frame.height)!)
+        return CGSize(width: reviewImageCollectionView.frame.width, height: reviewImageCollectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
