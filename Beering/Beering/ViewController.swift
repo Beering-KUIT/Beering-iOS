ce4279f17d7a14b6113d764ce3ccf411bb1b1cd4
jikbb2 <jikbb2@gmail.com>
2023-08-11 06:51:22 +0900

    Feat: LoginRequest 를 Main 으로 구현

--- Beering/Beering/ViewController.swift
+++ Beering/Beering/ViewController.swift
@@ -10,10 +10,19 @@ import SwiftUI
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginMain: UILabel!
+    @IBOutlet weak var LoginRequestButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainTextInit()
+        loginRequestButtonInit()
+        loginSkipButtonInit()
+        
 
-        self.view.backgroundColor = .yellow
+//        self.view.backgroundColor = .yellow
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -21,17 +30,30 @@ class ViewController: UIViewController {
 
     }
     
+    func mainTextInit(){
+        loginMain.text = "로그인하고\n나만의\n주류기록 남기기"
+        loginMain.textColor = BeeringColor.Black
+    }
+    
+    func loginRequestButtonInit(){
+        LoginRequestButton.setTitle("로그인할게요", for: .normal)
+        LoginRequestButton.layer.cornerRadius = 0
+        LoginRequestButton.layer.backgroundColor = BeeringColor.Black.cgColor
+        LoginRequestButton.layer.shadowOpacity = 0
+        LoginRequestButton.layer.shadowColor = nil
+    }
+    
+    func loginSkipButtonInit(){
+        skipButton.setTitle("먼저 둘러볼게요", for: .normal)
+        skipButton.setUnderLine()
+    }
+    
     @IBAction func loginRequestBtnTap(_ sender: Any) {
         
         let LoginStoryboard = UIStoryboard(name: "Login", bundle: nil)
-        let LoginVC = LoginStoryboard.instantiateInitialViewController()
-        // 메인 화면을 풀 스크린으로 표시
-        LoginVC?.modalPresentationStyle = .fullScreen
-        self.present(LoginVC!, animated: true)
-        
-        // 윈도우의 rootViewController로 설정하여 메인 화면 표시
-//        UIApplication.shared.windows.first?.rootViewController = LoginVC
-//        UIApplication.shared.windows.first?.makeKeyAndVisible()
+        let LoginVC = LoginStoryboard.instantiateViewController(withIdentifier: "Login")
+        self.navigationItem.backButtonTitle = ""
+        self.navigationController?.pushViewController(LoginVC, animated: true)
 
     }
     
@@ -52,9 +74,17 @@ class ViewController: UIViewController {
     
 }
 
-struct VCPreView:PreviewProvider {
-    static var previews: some View {
-        ViewController().toPreview()
+extension UIButton{
+    func setUnderLine(){
+        guard let title = title(for: .normal) else {return}
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: title.count))
+        setAttributedTitle(attributedString, for: .normal)
     }
 }
+//struct VCPreView:PreviewProvider {
+//    static var previews: some View {
+//        ViewController().toPreview()
+//    }
+//}
 
